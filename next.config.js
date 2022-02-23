970b5d90212c0551f7ad5c6e3b4afe451d0d54ec
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 18:43:20 -0300

    config next

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    path: 'https://ariannoel.com',
-    mongodburl: 'mongodb+srv://ariannoel:xkIDZb6tUyIXLUFd@cluster0.9uc6m.mongodb.net/ariannoel?retryWrites=true&w=majority'
+    path: 'https://ariannoel.com'
   }
 }
 
