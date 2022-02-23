e58aedaf870703d3b437b9ad252afa784da3d343
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 00:05:15 -0300

    probando puppeteer-core

--- pages/api/services/cv_pdf.js
+++ pages/api/services/cv_pdf.js
@@ -1,70 +1,51 @@
 import stream from 'stream'
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer-core'
 import hb from 'handlebars'
 import inlineCss from 'inline-css'
 import Promise from 'bluebird'
 const options = { format: 'A4', landscape: false, scale: 0.70, printBackground: true }
 const file = { url: 'https://ariannoel.com/cv_format' }
 
-// async function generatePdf(file, options, callback) {
-//   // we are using headless mode
-//   let args = [
-//     '--no-sandbox',
-//     '--disable-setuid-sandbox',
-//   ];
-//   if(options.args) {
-//     args = options.args;
-//     delete options.args;
-//   }
-
-//   const browser = await puppeteer.launch({
-//     args: args
-//   });
-//   const page = await browser.newPage();
-
-//   if(file.content) {
-//     data = await inlineCss(file.content, {url:"/"});
-//     console.log("Compiling the template with handlebars")
-//     // we have compile our code with handlebars
-//     const template = hb.compile(data, { strict: true });
-//     const result = template(data);
-//     const html = result;
-
-//     // We set the page content as the generated html by handlebars
-//     await page.setContent(html, {
-//       waitUntil: 'networkidle0', // wait for page to load completely
-//     });
-//   } else {
-//     await page.goto(file.url, {
-//       waitUntil:[ 'load', 'networkidle0'], // wait for page to load completely
-//     });
-//   }
-
-//   return Promise.props(page.pdf(options))
-//     .then(async function(data) {
-//        await browser.close();
-
-//        return Buffer.from(Object.values(data));
-//     }).asCallback(callback);
-// }
-
 async function generatePdf(file, options, callback) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
+  // we are using headless mode
+  let args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ];
+  if(options.args) {
+    args = options.args;
+    delete options.args;
+  }
 
-  // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio,
-    };
+  const browser = await puppeteer.launch({
+    args: args
   });
+  const page = await browser.newPage();
 
-  console.log('Dimensions:', dimensions);
+  if(file.content) {
+    data = await inlineCss(file.content, {url:"/"});
+    console.log("Compiling the template with handlebars")
+    // we have compile our code with handlebars
+    const template = hb.compile(data, { strict: true });
+    const result = template(data);
+    const html = result;
 
-  await browser.close();
+    // We set the page content as the generated html by handlebars
+    await page.setContent(html, {
+      waitUntil: 'networkidle2', // wait for page to load completely
+    });
+  } else {
+    await page.goto(file.url, {
+      waitUntil:[ 'load', 'networkidle2'], // wait for page to load completely
+    });
+  }
+
+  return Promise.props(page.pdf(options))
+    .then(async function(data) {
+       await browser.close();
+
+       return Buffer.from(Object.values(data));
+    }).asCallback(callback);
 }
 
 export default async function handler (req, res) {
