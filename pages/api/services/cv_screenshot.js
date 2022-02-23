908aaa36fb971332239e24aeb6206d025383c0fd
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 01:44:54 -0300

    chromium problem

--- pages/api/services/cv_screenshot.js
+++ pages/api/services/cv_screenshot.js
@@ -9,19 +9,19 @@ const file = { url: 'https://ariannoel.com/cv_format' }
 async function getBrowserInstance() {
 	const executablePath = await chromium.executablePath
 
-	if (!executablePath) {
-		// running locally
-		const puppeteer = require('puppeteer')
-		return puppeteer.launch({
-			args: chromium.args,
-			headless: true,
-			defaultViewport: {
-				width: 1280,
-				height: 720
-			},
-			ignoreHTTPSErrors: true
-		})
-	}
+	// if (!executablePath) {
+	// 	// running locally
+	// 	const puppeteer = require('puppeteer')
+	// 	return puppeteer.launch({
+	// 		args: chromium.args,
+	// 		headless: true,
+	// 		defaultViewport: {
+	// 			width: 1280,
+	// 			height: 720
+	// 		},
+	// 		ignoreHTTPSErrors: true
+	// 	})
+	// }
 
 	return chromium.puppeteer.launch({
 		args: chromium.args,
