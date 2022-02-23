c3c1a0564bd27be24241944119f35a37ba1420ef
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 01:52:57 -0300

    chromium problem resolve

--- pages/api/services/cv_pdf.js
+++ pages/api/services/cv_pdf.js
@@ -1,10 +1,39 @@
+import chromium from 'chrome-aws-lambda'
 import stream from 'stream'
-import puppeteer from 'puppeteer'
-import hb from 'handlebars'
-import inlineCss from 'inline-css'
+// import hb from 'handlebars'
+// import inlineCss from 'inline-css'
 import Promise from 'bluebird'
 const options = { format: 'A4', landscape: false, scale: 0.70, printBackground: true }
 const file = { url: 'https://ariannoel.com/cv_format' }
+
+async function getBrowserInstance() {
+	const executablePath = await chromium.executablePath
+
+	if (!executablePath) {
+		// running locally
+		const puppeteer = require('puppeteer')
+		return puppeteer.launch({
+			args: chromium.args,
+			headless: true,
+			defaultViewport: {
+				width: 1280,
+				height: 720
+			},
+			ignoreHTTPSErrors: true
+		})
+	}
+
+	return chromium.puppeteer.launch({
+		args: chromium.args,
+		defaultViewport: {
+			width: 1280,
+			height: 720
+		},
+		executablePath,
+		headless: chromium.headless,
+		ignoreHTTPSErrors: true
+	})
+}
 
 async function generatePdf(file, options, callback) {
   // we are using headless mode
@@ -17,28 +46,26 @@ async function generatePdf(file, options, callback) {
     delete options.args;
   }
 
-  const browser = await puppeteer.launch({
-    args: args,
-  });
+  const browser = await getBrowserInstance();
   const page = await browser.newPage();
 
-  if(file.content) {
-    data = await inlineCss(file.content, {url:"/"});
-    console.log("Compiling the template with handlebars")
-    // we have compile our code with handlebars
-    const template = hb.compile(data, { strict: true });
-    const result = template(data);
-    const html = result;
+  // if(file.content) {
+  //   data = await inlineCss(file.content, {url:"/"});
+  //   console.log("Compiling the template with handlebars")
+  //   // we have compile our code with handlebars
+  //   const template = hb.compile(data, { strict: true });
+  //   const result = template(data);
+  //   const html = result;
 
-    // We set the page content as the generated html by handlebars
-    await page.setContent(html, {
-      waitUntil: 'networkidle2', // wait for page to load completely
-    });
-  } else {
+  //   // We set the page content as the generated html by handlebars
+  //   await page.setContent(html, {
+  //     waitUntil: 'networkidle2', // wait for page to load completely
+  //   });
+  // } else {
     await page.goto(file.url, {
       waitUntil:[ 'load', 'networkidle2'], // wait for page to load completely
     });
-  }
+  // }
 
   return Promise.props(page.pdf(options))
     .then(async function(data) {
