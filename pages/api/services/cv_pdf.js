0860a2b16b359af998fcd6a7ca6aade978a0f132
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 00:42:31 -0300

    chromium problem

--- pages/api/services/cv_pdf.js
+++ pages/api/services/cv_pdf.js
@@ -1,7 +1,53 @@
 import stream from 'stream'
-import { generatePdf } from 'html-pdf-node'
+import puppeteer from 'puppeteer'
+import hb from 'handlebars'
+import inlineCss from 'inline-css'
+import Promise from 'bluebird'
+import chromium from 'chrome-aws-lambda'
 const options = { format: 'A4', landscape: false, scale: 0.70, printBackground: true }
 const file = { url: 'https://ariannoel.com/cv_format' }
+
+async function generatePdf(file, options, callback) {
+  // we are using headless mode
+  let args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ];
+  if(options.args) {
+    args = options.args;
+    delete options.args;
+  }
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args
+  });
+  const page = await browser.newPage();
+
+  if(file.content) {
+    data = await inlineCss(file.content, {url:"/"});
+    console.log("Compiling the template with handlebars")
+    // we have compile our code with handlebars
+    const template = hb.compile(data, { strict: true });
+    const result = template(data);
+    const html = result;
+
+    // We set the page content as the generated html by handlebars
+    await page.setContent(html, {
+      waitUntil: 'networkidle2', // wait for page to load completely
+    });
+  } else {
+    await page.goto(file.url, {
+      waitUntil:[ 'load', 'networkidle2'], // wait for page to load completely
+    });
+  }
+
+  return Promise.props(page.pdf(options))
+    .then(async function(data) {
+       await browser.close();
+
+       return Buffer.from(Object.values(data));
+    }).asCallback(callback);
+}
 
 export default async function handler (req, res) {
   try {
