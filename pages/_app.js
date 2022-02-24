08e184ddd1b7ce0e90925f19e57f6fe5991a1583
Arián Fernández <arianfernandez@gmail.com>
2022-02-23 21:41:40 -0300

    NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,18 @@
 import '../styles/globals.css'
+import NProgress from 'nprogress';
+import "../node_modules/nprogress/nprogress.css";
+import Router from 'next/router';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
