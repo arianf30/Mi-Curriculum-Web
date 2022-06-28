da8b7cb1113dc4ad9954deb62bb16a605a7efc6b
ArianDHNN <arian.fernandez@dhnn.com>
2022-06-28 16:54:32 -0300

    Fix behavior

--- components/RightColumn/index.js
+++ components/RightColumn/index.js
@@ -1,101 +1,169 @@
-import timeAgo from '../../utils/timeAgo'
-import GridTecnologies from '../GridTecnologies'
+import timeAgo from "../../utils/timeAgo";
+import GridTecnologies from "../GridTecnologies";
 
-export default function RightColumn ({ profile }) {
-  const { about, experience, cvInfo } = profile
+export default function RightColumn({ profile }) {
+  const { about, experience, cvInfo } = profile;
   return (
-    <div className='flex-auto p-8 bg-arian-grisoscuro2 text-arian-blanco2 max-h-max md:max-h-screen md:overflow-y-auto border-r border-arian-grisclaro scrollbar-thin scrollbar-thumb-arian-scroll scrollbar-track-arian-grisoscuro2'>
-
+    <div className="flex-auto p-8 bg-arian-grisoscuro2 text-arian-blanco2 max-h-max md:max-h-screen md:overflow-y-auto border-r border-arian-grisclaro scrollbar-thin scrollbar-thumb-arian-scroll scrollbar-track-arian-grisoscuro2">
       {/* Lines - imports */}
-      <p className='font-mono text-sm font-medium tracking-wide'><font className='text-arian-amarillo'>import</font> <font className='text-arian-rosa'>AboutUs</font> <font className='text-arian-amarillo'>from</font> <font className='text-arian-verde'>&#39;./components/AboutUs&#39;</font></p>
-      <p className='font-mono text-sm font-medium tracking-wide'><font className='text-arian-amarillo'>import</font> <font className='text-arian-rosa'>Experience</font> <font className='text-arian-amarillo'>from</font> <font className='text-arian-verde'>&#39;./components/Experience&#39;</font></p>
-      <p className='font-mono text-sm font-medium tracking-wide'><font className='text-arian-amarillo'>import</font> <font className='text-arian-rosa'>ThisApp</font> <font className='text-arian-amarillo'>from</font> <font className='text-arian-verde'>&#39;./components/ThisApp&#39;</font></p>
+      <p className="font-mono text-sm font-medium tracking-wide">
+        <font className="text-arian-amarillo">import</font>{" "}
+        <font className="text-arian-rosa">AboutUs</font>{" "}
+        <font className="text-arian-amarillo">from</font>{" "}
+        <font className="text-arian-verde">&#39;./components/AboutUs&#39;</font>
+      </p>
+      <p className="font-mono text-sm font-medium tracking-wide">
+        <font className="text-arian-amarillo">import</font>{" "}
+        <font className="text-arian-rosa">Experience</font>{" "}
+        <font className="text-arian-amarillo">from</font>{" "}
+        <font className="text-arian-verde">
+          &#39;./components/Experience&#39;
+        </font>
+      </p>
+      <p className="font-mono text-sm font-medium tracking-wide">
+        <font className="text-arian-amarillo">import</font>{" "}
+        <font className="text-arian-rosa">ThisApp</font>{" "}
+        <font className="text-arian-amarillo">from</font>{" "}
+        <font className="text-arian-verde">&#39;./components/ThisApp&#39;</font>
+      </p>
       <br />
 
       {/* Line - function App */}
-      <p className='font-mono text-sm font-medium tracking-wide'>
-        <font className='text-arian-amarillo'>export default</font> <font className='text-arian-celeste'><i>function</i></font> <font className='text-arian-azul'>App</font> <font className='text-arian-grisclaro2'>() {'{'}</font>
+      <p className="font-mono text-sm font-medium tracking-wide">
+        <font className="text-arian-amarillo">export default</font>{" "}
+        <font className="text-arian-celeste">
+          <i>function</i>
+        </font>{" "}
+        <font className="text-arian-azul">App</font>{" "}
+        <font className="text-arian-grisclaro2">() {"{"}</font>
         <br />
-        <font className='text-arian-amarillo pl-4'>return</font> <font className='text-arian-grisclaro2'>(</font>
+        <font className="text-arian-amarillo pl-4">return</font>{" "}
+        <font className="text-arian-grisclaro2">(</font>
       </p>
 
       <br />
       {/* Line - Component AboutUs */}
-      <p className='font-mono text-sm font-medium tracking-wide xl:pl-8'>
-        <font className='text-arian-azul'>&#60;AboutUs</font> <font className='text-arian-amarillo'><i>title</i></font><font className='text-arian-grisclaro2'>=</font><font className='text-arian-verde'>&#34;Acerca de mi&#34;</font> <font className='text-arian-azul'>/&#62;</font>
+      <p className="font-mono text-sm font-medium tracking-wide xl:pl-8">
+        <font className="text-arian-azul">&#60;AboutUs</font>{" "}
+        <font className="text-arian-amarillo">
+          <i>title</i>
+        </font>
+        <font className="text-arian-grisclaro2">=</font>
+        <font className="text-arian-verde">&#34;Acerca de mi&#34;</font>{" "}
+        <font className="text-arian-azul">/&#62;</font>
       </p>
 
       <br />
       {/* About Me */}
-      <div className='font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6'>
-        {about.map((p, index) => (
-          <p className='mb-3' key={`about-${index}`}>{p}</p>
+      <div className="font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6">
+        {about?.map((p, index) => (
+          <p className="mb-3" key={`about-${index}`}>
+            {p}
+          </p>
         ))}
       </div>
 
-      <div className='flex items-baseline m-8 mb-4 border-b border-arian-grisclaro' />
+      <div className="flex items-baseline m-8 mb-4 border-b border-arian-grisclaro" />
 
       <br />
       {/* Line - Component Experience */}
-      <p className='font-mono text-sm font-medium tracking-wide xl:pl-8'>
-        <font className='text-arian-azul'>&#60;Experience</font> <font className='text-arian-amarillo'><i>title</i></font><font className='text-arian-grisclaro2'>=</font><font className='text-arian-verde'>&#34;Experiencia Laboral&#34;</font>  <font className='text-arian-azul'>/&#62;</font>
+      <p className="font-mono text-sm font-medium tracking-wide xl:pl-8">
+        <font className="text-arian-azul">&#60;Experience</font>{" "}
+        <font className="text-arian-amarillo">
+          <i>title</i>
+        </font>
+        <font className="text-arian-grisclaro2">=</font>
+        <font className="text-arian-verde">
+          &#34;Experiencia Laboral&#34;
+        </font>{" "}
+        <font className="text-arian-azul">/&#62;</font>
       </p>
 
       {/* Experience */}
-      <div className='font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6'>
-        {experience.map((item, index) => {
-          const paragraphs = item.info.paragraphs
-          const works = item.info.worksLinks
+      <div className="font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6">
+        {experience?.map((item, index) => {
+          const paragraphs = item.info;
           return (
-            <div className="mt-10" key={`experience-${index}`}>
-            <div className='flex items-baseline mb-4'>
-              {/* <div className='bg-arian-grisclaro2 rounded-full h-2 w-2' /> */}
-                <div className='flex-1 mt-1 text-base font-medium text-arian-blanco'>{item.companyLink ? (<a href={item.companyLink} className='italic hover:text-arian-lila transition duration-300 ease-in-out' target="_blank" rel="noreferrer">{item.companyName}</a>) : item.companyName} <font className='text-xs text-arian-grisclaro2'>|{ (item.companyTimeFinished === 'Now' ? ' actualidad · ' : '') }{item.companyTimeInit && item.companyTimeFinished && timeAgo(item.companyTimeInit, item.companyTimeFinished)}</font></div>
+            <div className="mt-6" key={`experience-${index}`}>
+              <div className="flex items-baseline mb-1">
+                {/* <div className='bg-arian-grisclaro2 rounded-full h-2 w-2' /> */}
+                <div className="flex-1 mt-1 text-base font-medium text-arian-blanco">
+                  {item.companyLink ? (
+                    <a
+                      href={item.companyLink}
+                      className="italic hover:text-arian-lila transition duration-300 ease-in-out"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {item.companyName}
+                    </a>
+                  ) : (
+                    item.companyName
+                  )}{" "}
+                  <font className="text-xs text-arian-grisclaro2">
+                    |
+                    {item.companyTimeFinished === "Now" ? " actualidad · " : ""}
+                    {item.companyTimeInit &&
+                      item.companyTimeFinished &&
+                      timeAgo(item.companyTimeInit, item.companyTimeFinished)}
+                  </font>
+                </div>
+              </div>
+              {paragraphs?.map((p, pInd) => (
+                <p className="mb-3" key={`experience-p-${index}-${pInd}`}>
+                  {p}
+                </p>
+              ))}
             </div>
-            {paragraphs.map((p, pInd) => (
-              <p className='mb-3' key={`experience-p-${index}-${pInd}`}>{p}</p>
-            ))}
-            {works &&
-                <p className='mb-3 text-left'>
-                Algunos trabajos:&nbsp;
-                {works.map((work, wInd) => (
-                  <span key={`experience-l-${index}-${wInd}`} className="inline-block">
-                    <a className="text-arian-lila italic inline-block" href={work.link} target="_blank" rel="noreferrer">{work.name}</a>,&nbsp;
-                  </span>
-                ))}
-                entre otros...
-              </p>
-            }
-            </div>
-          )
+          );
         })}
       </div>
 
-      <div className='flex items-baseline m-8 mt-4 mb-6 pb-6 border-b border-arian-grisclaro' />
+      <div className="flex items-baseline m-8 mt-4 mb-6 pb-6 border-b border-arian-grisclaro" />
 
       <br />
       {/* Line - Component ThisApp */}
-      <p className='font-mono text-sm font-medium tracking-wide xl:pl-8'>
-        <font className='text-arian-azul'>&#60;ThisApp</font> <font className='text-arian-amarillo'><i>title</i></font><font className='text-arian-grisclaro2'>=</font><font className='text-arian-verde'>&#34;¿Por qué realicé una web curriculum?&#34;</font>  <font className='text-arian-azul'>/&#62;</font>
+      <p className="font-mono text-sm font-medium tracking-wide xl:pl-8">
+        <font className="text-arian-azul">&#60;ThisApp</font>{" "}
+        <font className="text-arian-amarillo">
+          <i>title</i>
+        </font>
+        <font className="text-arian-grisclaro2">=</font>
+        <font className="text-arian-verde">
+          &#34;¿Por qué realicé una web curriculum?&#34;
+        </font>{" "}
+        <font className="text-arian-azul">/&#62;</font>
       </p>
 
       <br />
       {/* This App */}
-      <div className='font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6'>
-        {cvInfo.paragraphs.map((p, index) => (
-          <p className='mb-3' key={`cvInfo-${index}`}>{p}</p>
+      <div className="font-body pl-0 pr-0 lg:pl-8 lg:pr-12 text-justify text-sm leading-6">
+        {cvInfo?.paragraphs?.map((p, index) => (
+          <p className="mb-3" key={`cvInfo-${index}`}>
+            {p}
+          </p>
         ))}
-        <p className='mb-3'>Mirá el proyecto en <a href={cvInfo.githubLink} target="_blank" className="underline decoration-4 cursor-pointer" rel="noreferrer">GitHub</a></p>
+        <p className="mb-3">
+          Mirá el proyecto en{" "}
+          <a
+            href={cvInfo?.githubLink}
+            target="_blank"
+            className="underline decoration-4 cursor-pointer"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
         <GridTecnologies />
       </div>
 
-      <p className='font-mono text-sm font-medium tracking-wide mt-5'>
+      <p className="font-mono text-sm font-medium tracking-wide mt-5">
         {/* Line 3 */}
-        <font className='text-arian-grisclaro2 pl-4'>)</font>
+        <font className="text-arian-grisclaro2 pl-4">)</font>
         <br />
         {/* Line 4 */}
-        <font className='text-arian-grisclaro2'>{'}'}</font>
+        <font className="text-arian-grisclaro2">{"}"}</font>
       </p>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,75 @@
-import Head from 'next/head'
-import Skill from '../models/Skill.js'
-import Profile from '../models/Profile.js'
-import SocialMenu from '../components/SocialMenu'
-import LeftColumn from '../components/LeftColumn'
-import RightColumn from '../components/RightColumn'
-import connectMongoClient from '../middleware/connectMongoClient.js'
-import { positions, Provider } from "react-alert"
-import AlertTemplate from "react-alert-template-basic"
+import Head from "next/head";
+import Skill from "../models/Skill.js";
+import Profile from "../models/Profile.js";
+import SocialMenu from "../components/SocialMenu";
+import LeftColumn from "../components/LeftColumn";
+import RightColumn from "../components/RightColumn";
+import connectMongoClient from "../middleware/connectMongoClient.js";
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 const options = {
   timeout: 5000,
-  position: positions.TOP_RIGHT
+  position: positions.TOP_RIGHT,
 };
 
-export default function Home ({ profile, skills }) {
-  const gP = JSON.parse(profile)
+export default function Home({ profile, skills }) {
+  const gP = JSON.parse(profile);
   const {
-    name, lastName, occupation, email, linkedin, github, instagram, avatar, about, experience, cvInfo
-  } = gP[0]
-  const getSkills = JSON.parse(skills)
+    name,
+    lastName,
+    occupation,
+    email,
+    linkedin,
+    github,
+    instagram,
+    avatar,
+    about,
+    experience,
+    cvInfo,
+  } = gP[0];
+  const getSkills = JSON.parse(skills);
   return (
     <Provider template={AlertTemplate} {...options}>
-      <div className='block md:flex md:h-screen font-sans min-h-screen bg-arian-negro'>
+      <div className="block md:flex md:h-screen font-sans min-h-screen bg-arian-negro">
         <Head>
           <title>Curriculum Web · Arián Noel Fernández</title>
-          <meta name="description" content="Siendo Web Developer, ¿por qué tendría un curriculum en PDF?" />
+          <meta
+            name="description"
+            content="Siendo Web Developer, ¿por qué tendría un curriculum en PDF?"
+          />
           <link rel="icon" href="/favicon.png" />
         </Head>
-        <div className='block md:w-1/2 md:h-full lg:w-auto lg:flex'>
+        <div className="block md:w-1/2 md:h-full lg:w-auto lg:flex">
           <SocialMenu profile={{ email, linkedin, github, instagram }} />
-          <LeftColumn profile={{ name, lastName, occupation, avatar }} skills={getSkills} />
+          <LeftColumn
+            profile={{ name, lastName, occupation, avatar }}
+            skills={getSkills}
+          />
         </div>
-        <div className='block md:flex-auto md:w-1/2'>
+        <div className="block md:flex-auto md:w-1/2">
           <RightColumn profile={{ about, experience, cvInfo }} />
         </div>
       </div>
     </Provider>
-  )
+  );
 }
 
-export async function getServerSideProps () {
-  const readAll = Promise.all([
-    Skill.find({}),
-    Profile.find({})
-  ])
-    .then(values => {
-      const readSkills = JSON.stringify(values[0])
-      const readProfile = JSON.stringify(values[1])
+export async function getServerSideProps() {
+  const readAll = Promise.all([Skill.find({}), Profile.find({})])
+    .then((values) => {
+      const readSkills = JSON.stringify(values[0]);
+      const readProfile = JSON.stringify(values[1]);
       return {
         profile: readProfile,
-        skills: readSkills
-      }
+        skills: readSkills,
+      };
     })
-    .catch(err => {
+    .catch((err) => {
       return {
-        error: `No fue posible conectarse con la BD: ${err}`
-      }
-    })
+        error: `No fue posible conectarse con la BD: ${err}`,
+      };
+    });
 
-  return { props: connectMongoClient(readAll) }
+  return { props: connectMongoClient(readAll) };
 }
